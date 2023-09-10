d3aede6d21a4ee708e6a96876e712d87330ba952
Ignatius Renaldy <irenaldyleonarto@gmail.com>
2023-09-11 00:15:12 +0700

    adjust initial template

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'HOL Introduction to Programming'
+copyright = '2023, Software Laboratory Center'
+author = 'Elizabeth Erika Felia Oroh ER22-2, Renaldy RX19-2'
 
 release = '0.1'
 version = '0.1.0'
@@ -32,4 +32,5 @@ templates_path = ['_templates']
 html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
+
 epub_show_urls = 'footnote'
